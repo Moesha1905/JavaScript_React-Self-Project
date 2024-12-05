f7c9ceeb2e466a8b3949487e9e4ec8c0eab986ad
MoeshaNovyna <141724948+MoeshaNovyna@users.noreply.github.com>
2024-12-05 14:29:25 +0800

    added success page

---
+++ my-new-app/src/Components/Success.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+export class Success extends Component {
+    continue = e => {
+        e.preventDefault();
+        // process form //
+        this.props.nextStep();
+
+    };
+
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    };
+
+  render() {
+    const { values: { firstName, lastName, email, occupation, city, bio} } = this.props;
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+            <AppBar title="Confirm User Data" />
+            <List>
+                <ListItem
+                    primaryText="First Name"
+                    secondaryText={ firstName } 
+                />
+                <ListItem
+                    primaryText="Last Name"
+                    secondaryText={ lastName } 
+                />
+                <ListItem
+                    primaryText="Email"
+                    secondaryText={ email } 
+                />
+                <ListItem
+                    primaryText="Occupation"
+                    secondaryText={ occupation } 
+                />
+                <ListItem
+                    primaryText="City"
+                    secondaryText={ city } 
+                />
+                <ListItem
+                    primaryText="Bio"
+                    secondaryText={ bio } 
+                />
+            </List>
+            <br/>
+            <RaisedButton
+                label="Confirm & Continue"
+                primary={true}
+                style={styles.button} 
+                onClick={this.continue}
+            />
+            <RaisedButton
+                label="Back"
+                primary={false}
+                style={styles.button} 
+                onClick={this.back}
+            />
+        </React.Fragment>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default Success;
