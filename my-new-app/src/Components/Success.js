54c9d790004ed506cfe58fabc1557d44e7d978d7
MoeshaNovyna <141724948+MoeshaNovyna@users.noreply.github.com>
2024-12-05 16:38:46 +0800

    added success message

--- my-new-app/src/Components/Success.js
+++ my-new-app/src/Components/Success.js
@@ -16,50 +16,12 @@ export class Success extends Component {
     };
 
   render() {
-    const { values: { firstName, lastName, email, occupation, city, bio} } = this.props;
     return (
       <MuiThemeProvider>
         <React.Fragment>
-            <AppBar title="Confirm User Data" />
-            <List>
-                <ListItem
-                    primaryText="First Name"
-                    secondaryText={ firstName } 
-                />
-                <ListItem
-                    primaryText="Last Name"
-                    secondaryText={ lastName } 
-                />
-                <ListItem
-                    primaryText="Email"
-                    secondaryText={ email } 
-                />
-                <ListItem
-                    primaryText="Occupation"
-                    secondaryText={ occupation } 
-                />
-                <ListItem
-                    primaryText="City"
-                    secondaryText={ city } 
-                />
-                <ListItem
-                    primaryText="Bio"
-                    secondaryText={ bio } 
-                />
-            </List>
-            <br/>
-            <RaisedButton
-                label="Confirm & Continue"
-                primary={true}
-                style={styles.button} 
-                onClick={this.continue}
-            />
-            <RaisedButton
-                label="Back"
-                primary={false}
-                style={styles.button} 
-                onClick={this.back}
-            />
+            <AppBar title="Success" />
+            <h1>Thank You For Your Submission</h1>
+            <p>You will get an email with further instructions</p>
         </React.Fragment>
       </MuiThemeProvider>
     )
