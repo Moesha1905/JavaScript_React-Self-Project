4b91f3ce6a43076626b5961225aa10f89559569d
MoeshaNovyna <141724948+MoeshaNovyna@users.noreply.github.com>
2024-12-05 10:09:17 +0800

    adding components details

---
+++ my-new-app/src/Components/FormUserDetails.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default FormUserDetails
