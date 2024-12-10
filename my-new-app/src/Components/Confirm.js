354321e76c9d4d51b113f7ecb77c9e35edb403be
MoeshaNovyna <141724948+MoeshaNovyna@users.noreply.github.com>
2024-12-10 17:08:41 +0800

    made changes to alignment

--- my-new-app/src/Components/Confirm.js
+++ my-new-app/src/Components/Confirm.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import { List, ListItem } from 'material-ui/List';
-import RaisedButton from 'material-ui/RaisedButton';
+import { AppBar, Button, Box, Grid, Typography } from '@mui/material';
 import * as XLSX from 'xlsx';
 
 export class Confirm extends Component {
     // Method to handle the confirmation and download action
-    continue = e => {
+    continue = (e) => {
         const { values } = this.props;
 
         // Show a confirmation prompt to the user
@@ -28,70 +25,152 @@ export class Confirm extends Component {
     };
 
     // Method to go back to the previous step
-    back = e => {
+    back = (e) => {
         e.preventDefault();
         this.props.prevStep();
     };
 
     render() {
-        const { values: { firstName, lastName, email, occupation, city, bio } } = this.props;
-        return (
-            <MuiThemeProvider>
-                <React.Fragment>
-                    <AppBar
-                        title="Confirm User Data"
-                        style={{ backgroundColor: '#0047AB' }}
-                    />
-                    <List>
-                        <ListItem
-                            primaryText="First Name"
-                            secondaryText={firstName}
-                        />
-                        <ListItem
-                            primaryText="Last Name"
-                            secondaryText={lastName}
-                        />
-                        <ListItem
-                            primaryText="Email"
-                            secondaryText={email}
-                        />
-                        <ListItem
-                            primaryText="Occupation"
-                            secondaryText={occupation}
-                        />
-                        <ListItem
-                            primaryText="City"
-                            secondaryText={city}
-                        />
-                        <ListItem
-                            primaryText="Bio"
-                            secondaryText={bio}
-                        />
-                    </List>
-                    <br />
-                    <RaisedButton
-                        label="Confirm & Continue"
-                        style={styles.button}
-                        backgroundColor="#0047AB"
-                        labelColor="#ffffff"
-                        onClick={this.continue}
-                    />
-                    <RaisedButton
-                        label="Back"
-                        style={styles.button}
-                        backgroundColor="#7393B3"
-                        labelColor="#ffffff"
-                        onClick={this.back}
-                    />
-                </React.Fragment>
-            </MuiThemeProvider>
-        );
-    }
-}
+        const {
+            values: { firstName, lastName, email, occupation, city, bio },
+        } = this.props;
 
-const styles = {
-    button: {
-        margin: 15
+        return (
+            <Box
+                sx={{
+                    padding: 3,
+                    maxWidth: 600,
+                    margin: 'auto',
+                    backgroundColor: '#f9f9f9',
+                    borderRadius: 2,
+                    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                }}
+            >
+                {/* AppBar with improved styling */}
+                <AppBar
+                    position="static"
+                    sx={{
+                        backgroundColor: '#0047AB',
+                        padding: 1,
+                        borderRadius: 1,
+                    }}
+                >
+                    <Typography variant="h6" align="center">
+                        Confirm User Data
+                    </Typography>
+                </AppBar>
+
+                {/* User Data Section */}
+                <Box sx={{ paddingY: 3 }}>
+                    <Grid container spacing={2}>
+                        {/* First Name */}
+                        <Grid item xs={4}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: 'bold', textAlign: 'right' }}
+                            >
+                                First Name:
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography variant="body1">{firstName}</Typography>
+                        </Grid>
+
+                        {/* Last Name */}
+                        <Grid item xs={4}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: 'bold', textAlign: 'right' }}
+                            >
+                                Last Name:
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography variant="body1">{lastName}</Typography>
+                        </Grid>
+
+                        {/* Email */}
+                        <Grid item xs={4}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: 'bold', textAlign: 'right' }}
+                            >
+                                Email:
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography variant="body1">{email}</Typography>
+                        </Grid>
+
+                        {/* Occupation */}
+                        <Grid item xs={4}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: 'bold', textAlign: 'right' }}
+                            >
+                                Occupation:
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography variant="body1">{occupation}</Typography>
+                        </Grid>
+
+                        {/* City */}
+                        <Grid item xs={4}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: 'bold', textAlign: 'right' }}
+                            >
+                                City:
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography variant="body1">{city}</Typography>
+                        </Grid>
+
+                        {/* Bio */}
+                        <Grid item xs={4}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: 'bold', textAlign: 'right' }}
+                            >
+                                Bio:
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography variant="body1">{bio}</Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+
+                {/* Buttons Section */}
+                <Box sx={{ marginTop: 3 }}>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{
+                            backgroundColor: '#0047AB',
+                            color: '#ffffff',
+                            marginBottom: 2,
+                        }}
+                        onClick={this.continue}
+                    >
+                        Confirm & Continue
+                    </Button>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{
+                            backgroundColor: '#7393B3',
+                            color: '#ffffff',
+                        }}
+                        onClick={this.back}
+                    >
+                        Back
+                    </Button>
+                </Box>
+            </Box>
+        );
     }
 }
 
