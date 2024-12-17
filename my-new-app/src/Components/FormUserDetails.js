963681d70d6f197681dac153464c593872e1fbfa
MoeshaNovyna <141724948+MoeshaNovyna@users.noreply.github.com>
2024-12-17 16:27:17 +0800

    created css file for user details

--- my-new-app/src/Components/FormUserDetails.js
+++ my-new-app/src/Components/FormUserDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Button, Box, Typography } from '@mui/material';
-import InputField from './InputField'; 
+import InputField from './InputField';
+import './FormUserDetails.css'; 
 
 const FormUserDetails = ({ values, handleChange, nextStep, errors }) => {
   const continueStep = (e) => {
@@ -9,9 +10,9 @@ const FormUserDetails = ({ values, handleChange, nextStep, errors }) => {
   };
 
   return (
-    <Box sx={{ padding: 2, maxWidth: 400, margin: 'auto' }}>
-      <AppBar position="static" sx={{ backgroundColor: '#0047AB', padding: 1 }}>
-        <Typography variant="h6" align="center">
+    <Box className="container"> 
+      <AppBar position="static" className="app-bar"> 
+        <Typography variant="h6" className="title">
           Enter User Details
         </Typography>
       </AppBar>
@@ -19,14 +20,14 @@ const FormUserDetails = ({ values, handleChange, nextStep, errors }) => {
         label="First Name"
         value={values.firstName}
         onChange={(e) => handleChange('firstName')(e)}
-        error = {!!errors.firstName}
+        error={!!errors.firstName}
         helperText={errors.firstName}
       />
       <InputField
         label="Last Name"
         value={values.lastName}
         onChange={(e) => handleChange('lastName')(e)}
-        error = {!!errors.lastName}
+        error={!!errors.lastName}
         helperText={errors.lastName}
       />
       <InputField
@@ -34,13 +35,13 @@ const FormUserDetails = ({ values, handleChange, nextStep, errors }) => {
         type="email"
         value={values.email}
         onChange={(e) => handleChange('email')(e)}
-        error = {!!errors.email}
+        error={!!errors.email}
         helperText={errors.email}
       />
       <Button
         fullWidth
         variant="contained"
-        sx={{ backgroundColor: '#0047AB', color: '#fff', marginTop: 2 }}
+        className="button"
         onClick={continueStep}
       >
         Continue
